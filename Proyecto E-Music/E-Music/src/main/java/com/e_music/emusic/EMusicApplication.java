40ae2d65de60f5139ab81bd5f0373e44df8fe625
Rxvargas92 <rodrigox.vargas@alumnos.frm.utn.edu.ar>
2022-10-03 19:25:22 -0300

    Create Proyect

---
+++ Proyecto E-Music/E-Music/src/main/java/com/e_music/emusic/EMusicApplication.java
@@ -0,0 +1,13 @@
+package com.e_music.emusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMusicApplication.class, args);
+	}
+
+}
---
+++ Proyecto E-Music/E-Music/src/test/java/com/e_music/emusic/EMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_music.emusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

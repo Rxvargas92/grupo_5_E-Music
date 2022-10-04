6c02f998833dab9200265c9bc3c59ebb12690a49
Rxvargas92 <rodrigox.vargas@alumnos.frm.utn.edu.ar>
2022-10-04 09:23:44 -0300

    Update Project

--- Proyecto E-Music/E-Music/src/main/java/com/e_music/emusic/EMusicApplication.java
+++ Proyecto E-Music/E-Music/src/main/java/com/e_music/emusic/EMusicApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EMusicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EMusicApplication.class, args);
-	}
+		SpringApplication.run(EMusicApplication.class, args);}
 
 }
